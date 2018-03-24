0bcb1ac69caa4994a6fa913456b9d5e2d26fa136
Miguel Moldes <miguel.moldes@brubank.com>
2018-03-24 20:24:50 -0300

    Taking out Pods from git

--- Pods/Target Support Files/Pods-MMPin/Pods-MMPin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MMPin : NSObject
-@end
-@implementation PodsDummy_Pods_MMPin
-@end
--- Pods/Target Support Files/Pods-MMPinTests/Pods-MMPinTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MMPinTests : NSObject
-@end
-@implementation PodsDummy_Pods_MMPinTests
-@end
